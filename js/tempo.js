ce5a92627f893255a70307f91c7d88dbbf8a807e
Rodrigo Abreu <rodrigo.telles.abreu@gmail.com>
2020-10-07 06:35:36 -0300

    Ajustando Relógio

---
+++ js/tempo.js
@@ -0,0 +1,20 @@
+let date = new Date();
+let hour = date.getHours();
+let min = date.getMinutes();
+let sec = date.getSeconds();
+let msec = date.getMilliseconds();
+let time = document.getElementById("tempo");
+time.innerHTML = `${hour}:${min}:${sec} `
+
+/* a linha abaixo atualiza a página de modo automático.
+document.location.reload(true);
+*/
+
+function inicio(){
+    let chour = 0 ;
+    let cmin = 0;
+    let csec = 0;
+    if (cmin == 0)
+}
+
+
